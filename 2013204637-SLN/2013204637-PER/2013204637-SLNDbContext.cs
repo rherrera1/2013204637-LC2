1c0029765962d5ae42ba765f534c97905d0d13e2
rherrera1 <rena2296@gmail.com>
2017-05-17 16:43:40 -0500

    3

--- 2013204637-SLN/2013204637-PER/2013204637-SLNDbContext.cs
+++ 2013204637-SLN/2013204637-PER/2013204637-SLNDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _2013204637;
+using _2013204637_PER.EntitiesConfigurations;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,5 +11,16 @@ namespace _2013204637_PER
 {
     class _2013204637_SLNDbContext : DbContext
     {
+        public DbSet<ATM> ATMs { get; set; }
+        public DbSet<BaseDatos> BaseDatos_s { get; set; }
+        public DbSet<Cuenta> Cuentas { get; set; }
+        public DbSet<DispensadorEfectivo> DispensadorEfectivo_s { get; set; }
+        public DbSet<Pantalla> Pantallas { get; set; }
+        public DbSet<RanuraDeposito> RanuraDeposito_s { get; set; }
+        public DbSet<Retiro> Retiros { get; set; }
+        public DbSet<Teclado> Teclados { get; set; }
+
+
+        
     }
 }
